a8b7eaab1b65b8fd367f1f0ddcd0e157a5b19869
IceBrick <91176400+IceBrick01@users.noreply.github.com>
2022-06-12 17:01:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,90 +1,184 @@
 #!/bin/bash
 printf "\033c"
 
-mcsmanager_install_path="/opt/mcsmtw"
-node_install_path="/opt/node-v14.17.6-linux-x64"
+# wget -N https://shell.ea0.cn/install_MCSManager.sh ; sudo bash install_MCSManager.sh
 
-# ----------------- Program start ----------------- 
+# 刪除自己
+rm -f "$0"
 
-# rm -f "$0"
+# 環境檢查
+Red_Error() {
+  echo '================================================='
+  printf '\033[1;31;40m%b\033[0m\n' "$@"
+  exit 1
+}
 
 if [ $(whoami) != "root" ]; then
-  Red_Error "[x] Please use Root!"
+  Red_Error "[x] 請使用 root 權限執行 MCSManager 安裝命令！"
 fi
 
 is64bit=$(getconf LONG_BIT)
 if [ "${is64bit}" != '64' ]; then
-  Red_Error "[x] Please use 64-bit system!"
+  Red_Error "[x] 當前面板版本不支持 32位 系統, 請使用 64位 系統！"
 fi
 
 
+
+# 開始安裝
 echo "+----------------------------------------------------------------------
-| MCSManager Installer
+| MCSManager TW FOR CentOS/Ubuntu/Debian
 +----------------------------------------------------------------------
-| Copyright © 2021 Suwings All rights reserved.
+| Copyright © 2017-2021 Suwings(MCSManager.com) All rights reserved.
 +----------------------------------------------------------------------
-| Shell Install Script by IceBrick
+| Shell Install Script by 冰磚(IceBrick)
 +----------------------------------------------------------------------
 "
+while [ "$go" != 'y' ] && [ "$go" != 'n' ]
+do
+	read -r -p "[-] 現在要安裝 MCSManager TW 到 \"/opt/mcsmtw\" 目錄嗎？(y/n): " go;
+done
 
-echo "[+] Installing dependent software... (git,tar)"
-yum install -y git tar
-apt install -y git tar
-pacman -Syu --noconfirm git tar
+if [ "$go" == 'n' ];then
+	exit;
+fi
 
-echo "[x] Delete the original MCSManager"
-rm -irf ${mcsmanager_install_path}
 
-echo "[+] mkdir -p ${mcsmanager_install_path}"
-mkdir -p ${mcsmanager_install_path} || exit
 
-echo "[→] cd ${mcsmanager_install_path}"
-cd ${mcsmanager_install_path} || exit
+# 定義 MCSManager 安裝目錄
+mcsmanager_install_path="/opt/mcsmtw"
 
-echo "[↓] git clone MCSManager/MCSManager-Daemon-Production.git"
-git clone https://gitee.com/mcsmanager/MCSManager-Daemon-Production.git
+# 定義 Node 安裝目錄
+node_install_path="/opt/node-v14.17.6-linux-x64"
 
-echo "[-] mv MCSManager-Daemon-Production daemon"
-mv MCSManager-Daemon-Production daemon
 
-echo "[→] cd daemon"
-cd daemon || exit
 
-echo "[+] npm install --registry=https://registry.npm.taobao.org"
-npm install --registry=https://registry.npm.taobao.org
+if [ -d $mcsmanager_install_path ];then
+  printf "\033c"
+	echo -e "----------------------------------------------------"
+  echo -e "檢查到已有 MCSManager TW 面版安裝在 \"$mcsmanager_install_path\""
+  echo -e "繼續安裝會刪除原有 MCSManager TW 面版的所有數據！"
+  echo -e "----------------------------------------------------"
+  echo ""
+  read -r -p "[-] 確認已瞭解以上內容，我確定已備份完成 (輸入yes繼續安裝): " yes;
 
-echo "[←] cd .."
-cd ..
+  if [ "$yes" != "yes" ];then
+  	echo -e "------------"
+  	echo "取消安裝"
+  	exit;
+  fi
+fi
 
-echo "[↓] git clone DreamStart-Team/MCSManager-Web-zhtw9.4.4.git"
-git clone https://github.com/DreamStart-Team/MCSManager-Web-zhtw9.4.4.git
 
-echo "[-] mv MCSManager-Web-zhtw9.4.4 web"
-mv MCSManager-Web-zhtw9.4.4 web
 
-echo "[→] cd web"
-cd web || exit
+if (systemctl -q is-active mcsm-daemon.service || systemctl -q is-active mcsm-web.service); then
+  echo "[-] MCSManager TW 服務正在運行，停止服務..."
+  systemctl stop mcsmtw-{daemon,web}.service
+  systemctl disable mcsmtw-{daemon,web}.service
+  echo
+fi
 
-echo "[+] npm install --registry=https://registry.npm.taobao.org"
-npm install --registry=https://registry.npm.taobao.org
 
-echo "=============== MCSManager ==============="
-echo " Daemon: ${mcsmanager_install_path}/daemon"
-echo " Web: ${mcsmanager_install_path}/web"
-echo "=============== MCSManager ==============="
-echo
-echo ""
-echo -e "\033[1;32m[ok] MCSManager installed successfully!!!\033[0m"
-echo "[ok] Location: ${mcsmanager_install_path}"
-echo
-sleep 3
 
-echo "[x] Initialize the service file"
-rm -f /etc/systemd/system/mcsmtw-daemon.service
-rm -f /etc/systemd/system/mcsmtw-web.service
+Install_Node() {
+  printf "\033c"
 
-echo "[+] cat >>/etc/systemd/system/mcsmtw-daemon.service"
-cat >>/etc/systemd/system/mcsmtw-daemon.service <<'EOF'
+  echo "[x] 刪除原有 Node 環境"
+  rm -irf ${node_install_path}
+
+  echo "[→] 進入 Node 安裝目錄"
+  cd /opt || exit
+
+  echo "[↓] 下載 Node v14.17.6 壓縮包..."
+  wget https://nodejs.org/download/release/v14.17.6/node-v14.17.6-linux-x64.tar.gz
+
+  echo "[↑] 解壓 node-v14.17.6-linux-x64.tar.gz"
+  tar -zxf node-v14.17.6-linux-x64.tar.gz
+
+  echo "[x] 刪除 node-v14.17.6-linux-x64.tar.gz"
+  rm -rf node-v14.17.6-linux-x64.tar.gz
+
+  echo "[x] 刪除原有 Node 鏈接"
+  rm -f /usr/bin/npm
+  rm -f /usr/bin/node
+  rm -f /usr/local/bin/npm
+  rm -f /usr/local/bin/node
+
+  echo "[+] 創建 Node 鏈接"
+  ln -s ${node_install_path}/bin/npm /usr/bin/
+  ln -s ${node_install_path}/bin/node /usr/bin/
+  ln -s ${node_install_path}/bin/npm /usr/local/bin/
+  ln -s ${node_install_path}/bin/node /usr/local/bin/
+
+  printf "\033c"
+  echo "=============== Node Version ==============="
+  echo " node: $(node -v)"
+  echo " npm: $(npm -v)"
+  echo "=============== Node Version ==============="
+  echo
+  echo "[-] Node 安裝完成，即將開始安裝 MCSManager TW..."
+  echo
+  sleep 3
+}
+
+Install_MCSManager() {
+  printf "\033c"
+
+  echo "[x] 刪除原有 MCSManager TW"
+  rm -irf ${mcsmanager_install_path}
+
+  echo "[+] 創建 MCSManager TW 安裝目錄"
+  mkdir -p ${mcsmanager_install_path} || exit
+
+  echo "[→] 進入 MCSManager TW 安裝目錄"
+  cd ${mcsmanager_install_path} || exit
+
+  echo "[↓] 下載 MCSManager CN 守護進程..."
+  git clone https://github.com/MCSManager/MCSManager-Daemon-Production.git
+
+  echo "[-] 重命名 Daemon -> daemon"
+  mv Daemon daemon
+
+  echo "[→] 進入 MCSManager-Daemon 目錄"
+  cd daemon || exit
+
+  echo "[+] 安裝 npm 依賴庫..."
+  npm install
+
+  echo "[←] 退出 MCSManager-Daemon 目錄"
+  cd ..
+
+  echo "[↓] 下載 MCSManager TW 網頁服務..."
+  git clone https://github.com/DreamStart-Team/MCSManager-Web-Production-tw9.4.5.git
+
+  echo "[-] 重命名 Web -> web"
+  mv Web web
+
+  echo "[→] 進入 MCSManager-Web 目錄"
+  cd web || exit
+
+  echo "[+] 安裝 npm 依賴庫..."
+  npm install
+
+  printf "\033c"
+  echo "=============== MCSManager TW ==============="
+  echo " Daemon(守護進程): ${mcsmanager_install_path}/daemon"
+  echo " Web(網頁服務): ${mcsmanager_install_path}/web"
+  echo "=============== MCSManager TW ==============="
+  echo
+  echo "[-] MCSManager TW 安裝完成，即將開始創建 MCSManager TW 系統服務..."
+  echo
+  sleep 3
+}
+
+Create_Service() {
+  printf "\033c"
+
+  echo "[x] 刪除 MCSManager TW 服務"
+  rm -f /etc/systemd/system/mcsmtw-daemon.service
+  rm -f /etc/systemd/system/mcsmtw-web.service
+
+  echo "[+] 創建 MCSManager-Daemon 服務"
+  cat >>/etc/systemd/system/mcsmtw-daemon.service <<'EOF'
 [Unit]
 Description=MCSManager Daemon
 
@@ -98,8 +192,8 @@ ExecStop=/bin/kill -s QUIT $MAINPID
 WantedBy=multi-user.target
 EOF
 
-echo "[+] cat >>/etc/systemd/system/mcsmtw-web.service"
-cat >>/etc/systemd/system/mcsmtw-web.service <<'EOF'
+  echo "[+] 創建 MCSManager-Web 服務"
+  cat >>/etc/systemd/system/mcsm-web.service <<'EOF'
 [Unit]
 Description=MCSManager Web
 
@@ -113,47 +207,65 @@ ExecStop=/bin/kill -s QUIT $MAINPID
 WantedBy=multi-user.target
 EOF
 
-echo "[-] systemctl daemon-reload"
-systemctl daemon-reload
+  echo "[-] 重載服務配置文件"
+  systemctl daemon-reload
 
-echo "[+] systemctl enable mcsmtw-daemon.service --now"
-systemctl enable mcsmtw-daemon.service --now
+  echo "[+] 啟用 MCSManager-Daemon 服務"
+  systemctl enable mcsmtw-daemon.service --now
 
-sleep 4
+  echo "[+] 啟用 MCSManager-Web 服務"
+  systemctl enable mcsmtw-web.service --now
 
-echo "[+] systemctl enable mcsmtw-web.service --now"
-systemctl enable mcsmtw-web.service --now
+  echo "[↓] 下載 MCSManager-命令行..."
+  rm -f /opt/mcsm.sh
+  wget -P /opt https://raw.githubusercontent.com/IceBrick01/MCSManager-Client/main/mcsm.sh
+  chmod -R 755 /opt/mcsm.sh
 
-sleep 4
-  
-echo "[↓] 下載 MCSManager Client..."
-rm -f /opt/mcsmtw.sh
-wget -P /opt https://raw.githubusercontent.com/IceBrick01/MCSManager-Client/main/mcsmtw.sh
-chmod -R 755 /opt/mcsmtw.sh
+  echo "[+] 創建 MCSManager-命令行 鏈接"
+  rm -f /usr/local/bin/mcsmtw
+  ln -s /opt/mcsm.sh /usr/local/bin/mcsmtw
 
-echo "[+] 安裝 MCSManager Client..."
-rm -f /usr/local/bin/mcsmtw
-ln -s /opt/mcsmtw.sh /usr/local/bin/mcsmtw
+  echo "[-] 正在檢查服務..."
 
-echo "=================================================================="
-echo -e "\033[1;32mMCSManager TW Client - 安裝成功\033[0m"
-echo "=================================================================="
-echo "您可以在命令行使用 \"mcsmtw\" 呼出 MCSManager TW Client"
-  
-echo "=================================================================="
-echo -e "\033[1;32mWelcome to MCSManager (WEB)\033[0m"
-echo "=================================================================="
-echo "Web Service Address:    http://localhost:23333"
-echo "Daemon Service Address: http://localhost:24444"
-echo "Username: root"
-echo "Password: 123456"
-echo -e "\033[33mEnglish: You must expose ports 23333 and 24444 to use the service properly on the Internet.\033[0m"
-echo -e "\033[33mChinese: 安装且启动完毕，您必须开放 23333 与 24444 端口来确保面板的正常使用。\033[0m"
-echo ""
-echo "=================================================================="
-echo "systemctl restart mcsmtw-{daemon,web}.service"
-echo "systemctl disable mcsmtw-{daemon,web}.service"
-echo "systemctl enable mcsmtw-{daemon,web}.service"
-echo "systemctl start mcsmtw-{daemon,web}.service"
-echo "systemctl stop mcsmtw-{daemon,web}.service"
-echo "=================================================================="
+  sleep 5
+
+  printf "\033c"
+  if (systemctl -q is-active mcsmtw-daemon.service && systemctl -q is-active mcsmtw-web.service); then
+    getIpAddress=$(curl -sS --connect-timeout 10 -m 60 https://shell.ea0.cn/api/getIpAddress)
+    LOCAL_IP=$(ip addr | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -E -v "^127\.|^255\.|^0\." | head -n 1)
+
+    echo "=================================================================="
+    echo -e "\033[1;32mMCSManager TW- 恭喜，安裝成功！\033[0m"
+    echo "=================================================================="
+    echo "網頁服務地址-內網: http://${LOCAL_IP}:23333"
+    echo "守護進程地址-內網: http://${LOCAL_IP}:24444"
+    echo "網頁服務地址: http://${getIpAddress}:23333"
+    echo "守護進程地址: http://${getIpAddress}:24444"
+    echo "默認賬號: root"
+    echo "默認密碼: 123456"
+    echo -e "\033[33m若無法訪問面板，請檢查防火牆/安全組是否有放行面板[23333/24444]埠\033[0m"
+    #echo "=================================================================="
+    #echo "重啟服務: systemctl restart mcsmtw-{daemon,web}.service"
+    #echo "禁用服務: systemctl disable mcsmtw-{daemon,web}.service"
+    #echo "啟用服務: systemctl enable mcsmtw-{daemon,web}.service"
+    #echo "啟動服務: systemctl start mcsmtw-{daemon,web}.service"
+    #echo "停止服務: systemctl stop mcsmtw-{daemon,web}.service"
+    echo "=================================================================="
+    echo "您可以在命令行使用 \"mcsmtw\" 呼出 MCSManager-命令行"
+  else
+    Red_Error "[x] 服務啟動失敗"
+  fi
+}
+
+
+
+printf "\033c"
+echo "[+] 安裝 git tar 包..."
+yum install -y git tar
+apt install -y git tar
+pacman -Syu --noconfirm git tar
+
+# 安裝
+Install_Node
+Install_MCSManager
+Create_Service
